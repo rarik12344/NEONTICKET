426cc2490fd3d334436c60373a8ecdbe82ee1fdf
rarik12344 <65827942+rarik12344@users.noreply.github.com>
2025-05-01 01:17:06 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,15 @@
-
+export const CONFIG = {
+  contractAddress: "0x6927648b3114B8B54FA5476Ec3BC3A52f1ab513B",
+  contractAbi: [
+    // ... ваш ABI контракта ...
+  ],
+  baseChainId: '0x2105',
+  baseRpcUrl: 'https://mainnet.base.org',
+  baseExplorerUrl: 'https://basescan.org',
+  currencySymbol: 'ETH',
+  defaultGasPrice: 0.2,
+  gasLimitBuffer: 1.2,
+  defaultGasLimit: 200000,
+  ethPriceUSD: 3000,
+  frameVersion: 'next'
+};
