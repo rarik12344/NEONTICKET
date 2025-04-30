f396b84084332881af9b482dbd18ee880dd8a6d2
rarik12344 <65827942+rarik12344@users.noreply.github.com>
2025-05-01 01:40:17 +0200

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,1 +1,16 @@
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
+import Head from "next/head";
 
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <script src="https://unpkg.com/@farcaster/frame-sdk@0.0.37/dist/frame-sdk.umd.js" async />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
