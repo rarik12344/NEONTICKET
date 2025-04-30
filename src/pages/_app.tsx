5402d3c680d1d60ffc17be3d9ba1a10d1e7eb1e5
rarik12344 <65827942+rarik12344@users.noreply.github.com>
2025-05-01 01:49:53 +0200

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,11 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <script src="https://unpkg.com/@farcaster/frame-sdk@0.0.37/dist/frame-sdk.umd.js" async />
+        <script 
+          src="https://unpkg.com/@farcaster/frame-sdk@0.0.37/dist/frame-sdk.umd.js" 
+          async 
+          data-domain="lotteryneon.vercel.app"
+        />
       </Head>
       <Component {...pageProps} />
     </>
