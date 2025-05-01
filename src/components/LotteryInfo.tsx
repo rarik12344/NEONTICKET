d0a4fc4a7bfbd07c526dff21ab5da620cd766867
rarik12344 <65827942+rarik12344@users.noreply.github.com>
2025-05-01 02:33:02 +0200

    Update LotteryInfo.tsx

--- src/components/LotteryInfo.tsx
+++ src/components/LotteryInfo.tsx
@@ -1,1 +1,21 @@
+import { CONFIG } from '../config';
 
+interface LotteryInfoProps {
+  currentPool: string;
+  ticketPrice: string;
+}
+
+export default function LotteryInfo({ currentPool, ticketPrice }: LotteryInfoProps) {
+  return (
+    <div className="space-y-2 my-4">
+      <div className="flex justify-between">
+        <span>Current Pool:</span>
+        <span>{currentPool} {CONFIG.currencySymbol}</span>
+      </div>
+      <div className="flex justify-between">
+        <span>Ticket Price:</span>
+        <span>{ticketPrice} {CONFIG.currencySymbol}</span>
+      </div>
+    </div>
+  );
+}
