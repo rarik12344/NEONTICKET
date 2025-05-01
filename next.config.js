340f4f38cb9ac5657645dbcf10cbc057d8cc8ca0
rarik12344 <65827942+rarik12344@users.noreply.github.com>
2025-05-02 01:04:11 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
     domains: ['i.ibb.co'],
   },
-};
+  headers: async () => [
+    {
+      source: '/.well-known/farcaster.json',
+      headers: [
+        {
+          key: 'Access-Control-Allow-Origin',
+          value: '*',
+        },
+        {
+          key: 'Content-Type',
+          value: 'application/json',
+        },
+      ],
+    },
+  ],
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
