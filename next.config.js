8e1239dd4021b9a39cf04b0ab84f3883fe0cc32e
rarik12344 <65827942+rarik12344@users.noreply.github.com>
2025-05-02 21:45:56 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,31 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
   images: {
-    domains: ['i.ibb.co', 'lotteryneon.vercel.app'],
-    unoptimized: true,
+    domains: ['i.ibb.co'],
   },
   async headers() {
     return [
       {
         source: '/.well-known/farcaster.json',
         headers: [
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Content-Type', value: 'application/json' },
-        ],
-      },
-      {
-        source: '/:path*',
-        headers: [
-          { 
-            key: 'X-Frame-Options', 
-            value: 'ALLOW-FROM https://warpcast.com' 
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Content-Type',
+            value: 'application/json',
           },
         ],
       },
-    ];
+    ]
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
